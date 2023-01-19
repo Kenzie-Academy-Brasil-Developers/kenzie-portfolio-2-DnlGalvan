1770533437e3cea8b0619b0f48d6945a95342851
Daniel Galvan <dgalvan@gmail.com>
2023-01-19 10:17:28 -0300

    refact: Alterando dados base stackData e userData

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
-  facebookUser: "",
+  nameUser: "Daniel Galvan",
+  githubUser: "DnlGalvan",
+  linkedinUser: "danielgalvan83",
+  whatsappNumber: "+5547999936241",
+  emailUser: "dgalvan@gmail.com",
+  instagramUser: "dnl.galvan",
+  facebookUser: "dnlgalvan",
 };
