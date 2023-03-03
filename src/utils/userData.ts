7bb21cb7e1c741d6d35d6f43fb903bf103acf2fd
Daniel Galvan <dgalvan@gmail.com>
2023-03-03 07:09:06 -0300

    altereção telefone

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -2,7 +2,7 @@ export const userData = {
   nameUser: "Daniel Galvan",
   githubUser: "DnlGalvan",
   linkedinUser: "danielgalvan83",
-  whatsappNumber: "+5547999936241",
+  whatsappNumber: "47999936241",
   emailUser: "dgalvan@gmail.com",
   instagramUser: "dnl.galvan",
   facebookUser: "dnlgalvan",
